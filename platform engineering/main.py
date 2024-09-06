53ee029444e3a60d4b70bc32c1ffac6fd5004c58
yair232 <yairginat23@gmail.com>
2024-09-06 17:43:41 +0300

    s3 update

--- platform engineering/main.py
+++ platform engineering/main.py
@@ -1,5 +1,6 @@
 import argparse
 import boto3
+import json
 
 def EC2(type, ami):
     if ami == 'amazon':
@@ -42,9 +43,6 @@ def stop_ec2_instance():
     else:
         print("no instance to stop")
 
-
-
-
 def list_my_ec2():
     client = boto3.client('ec2')
     resp = client.describe_instances()
@@ -67,11 +65,45 @@ def start_ec2_instance():
         print("no instance to start")
 
 
+def create_s3(sure):
+    s3 = boto3.client('s3')
+    bucket_name='yaircli'
+    s3.create_bucket(Bucket=bucket_name)
+    if sure == 'yes':
+        bucket_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "PublicReadGetObject",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": "s3:GetObject",
+                "Resource": f"arn:aws:s3:::{bucket_name}/*"
+            }
+        ]
+    }
+        bucket_policy_json = json.dumps(bucket_policy)
+        s3 = boto3.client('s3')
+        s3.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy_json)
+    print("S3 bucket created successfully")
+
+
+
+def upload(file,object_name):
+    s3 = boto3.client('s3')
+    bucket_name = 'yaircli'
+    try:
+        s3.upload_file(file, bucket_name, object_name)
+        print(f"File '{file}' uploaded to '{bucket_name}/{object_name}'")
+    except Exception as e:
+        print(f"Error uploading file: {e}")
+
+
 def main():
     parser = argparse.ArgumentParser(description='Manage AWS resources.')
     parser.add_argument('-r', '--resource', required=True, choices=['ec2', 's3', 'route53'],
                         help='Type of AWS resource to manage.')
-    parser.add_argument('-a', '--action', required=True, choices=['create', 'manage', 'list'],
+    parser.add_argument('-a', '--action', required=True, choices=['create', 'manage', 'list','upload'],
                         help='Action to perform on the specified resource.')
     parser.add_argument('-t', '--type', choices=['t2.nano', 't4g.nano'],
                         help='Type of EC2 instance (required for EC2 create action).')
@@ -79,6 +111,10 @@ def main():
                         help='Image for EC2 instance (required for EC2 create action).')
     parser.add_argument( '--status', choices=['start', 'stop'],
                         help='Specify whether to start or stop the EC2 instance.')
+    parser.add_argument('--choice', choices=['public', 'private'],
+                        help='choice between public and private access.')
+    parser.add_argument('--file', help='Path to the file you want to upload')
+    parser.add_argument('--name', help='give a name for the file')
     args = parser.parse_args()
     if args.resource == 'ec2':
         if args.action == 'create':
@@ -92,10 +128,25 @@ def main():
                 stop_ec2_instance()
             elif args.status == 'start':
                 start_ec2_instance()
-        else:
+        elif args.action == 'list':
             list_id = list_my_ec2()
             print(list_id)
             
+    if args.resource == 's3':
+        if args.action == 'create':
+            if not args.choice:
+                parser.error("--choice isr required for creating s3 bucket")
+            if args.choice == 'public':
+                sure = input("are you sure?")
+                if sure == 'yes':
+                    create_s3(sure)
+            else:
+                create_s3(sure='no')
+        elif args.action == 'upload':
+            if args.file == '' or not args.name:
+                parser.error("The --file argument cannot be an empty string and --name are required.")
+            upload(args.file,args.name)
+
 
 
 main()
